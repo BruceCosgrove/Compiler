9635318348b66d0ecf8e1bc89075b6bbfc73492e
Shlayne <1901.brc@gmail.com>
2024-01-03 23:03:08 -0800

    Simplify tokenizer.

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -9,59 +9,43 @@ namespace shl
     {
         std::vector<token> tokens;
 
-        size_type token_begin_index = 0;
-        size_type token_end_index = 0;
-
         while (auto c = peek())
         {
-            if (std::isspace(c.value()))
-            {
-                consume();
+            if (try_consume_token_value(c, &std::isspace))
                 continue;
-            }
-            if (std::isalpha(c.value()))
+            if (auto token_value = try_consume_token_value(c, &std::isalpha, &std::isalnum))
             {
-                token_begin_index = index();
-                consume();
-                token_end_index = index();
-
-                for (c = peek(); c && std::isalnum(c.value()); c = peek())
-                {
-                    ++token_end_index;
-                    consume();
-                }
-
-                auto token_value = std::string_view(container()).substr(token_begin_index, token_end_index - token_begin_index);
                 if (token_value == "return") tokens.emplace_back(token_type::_return);
                 else error_exit("Invalid identifier.");
             }
-            else if (std::isdigit(c.value()))
-            {
-                token_begin_index = index();
-                consume();
-                token_end_index = index();
-
-                while ((c = peek()) && std::isdigit(c.value()))
-                {
-                    ++token_end_index;
-                    consume();
-                }
-
-                auto token_value = std::string_view(container()).substr(token_begin_index, token_end_index - token_begin_index);
+            else if (auto token_value = try_consume_token_value(c, &std::isdigit, &std::isdigit))
                 tokens.emplace_back(token_type::_integer_literal, token_value);
-            }
-            else if (c == ';')
-            {
-                // token_begin_index = index();
-                consume();
-                // token_end_index = index();
-
-                // auto token_value = std::string_view(container()).substr(token_begin_index, token_end_index - token_begin_index);
+            else if (try_consume_token_value(c, [](int c) -> int { return c == ';'; }))
                 tokens.emplace_back(token_type::_semicolon);
-            }
             else error_exit("Invalid character.");
         }
 
         return tokens;
+    }
+
+    std::optional<std::string_view> tokenizer::try_consume_token_value(std::optional<char>& c, const token_pred first_pred, const token_pred rest_pred)
+    {
+        if (!first_pred(c.value()))
+            return std::nullopt;
+
+        const size_type token_begin_index = index();
+        consume();
+        size_type token_end_index = index();
+
+        if (rest_pred)
+        {
+            while ((c = peek()) && rest_pred(c.value()))
+            {
+                ++token_end_index;
+                consume();
+            }
+        }
+
+        return std::string_view(container()).substr(token_begin_index, token_end_index - token_begin_index);
     }
 } // namespace shl
--- src/tokenizer.hpp
+++ src/tokenizer.hpp
@@ -2,6 +2,7 @@
 
 #include "tokens.hpp"
 #include "consumer.hpp"
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -14,5 +15,9 @@ namespace shl
         [[nodiscard]] inline tokenizer(std::string&& source_code) noexcept : consumer<std::string>(std::move(source_code)) {}
 
         [[nodiscard]] std::vector<token> tokenize();
+
+    private:
+        using token_pred = int(*)(int);
+        [[nodiscard]] std::optional<std::string_view> try_consume_token_value(std::optional<char>& c, const token_pred first_pred, const token_pred rest_pred = nullptr);
     };
 } // namespace shl
